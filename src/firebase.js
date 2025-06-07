13e64a2c3075bdce22bb57c5567869a38d90faa4
biro425 <smntlf425@gmail.com>
2025-06-07 14:09:55 +0900

    feat: dshs.kr 도메인용 구글 로그인 페이지 구현

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9yvIHfOmrIV-KLpm_BBEDG0VFrUnf7Z0",
+    authDomain: "dshs-f8a92.firebaseapp.com",
+    projectId: "dshs-f8a92",
+    storageBucket: "dshs-f8a92.firebasestorage.app",
+    messagingSenderId: "736840004259",
+    appId: "1:736840004259:web:2f6e89f3bcdbf431970516",
+    measurementId: "G-WKNY95BRGY"
+  };
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firebase auth와 provider 객체 내보내기
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(); 
